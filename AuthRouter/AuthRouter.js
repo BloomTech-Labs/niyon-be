0bcb73823c8ba145506d601d7bb152586c6024fb
Joe Thompson <joe@localhost.localdomain>
2020-05-11 22:03:19 -0400

    added registration route
    Co-authored-by: Tim Mitzen <tim.mitzen@gmail.com>
    Co-authored-by: Joe Thompson <bmo3696@gmail.com>

--- AuthRouter/AuthRouter.js
+++ AuthRouter/AuthRouter.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const helpers = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
+router.post('/', async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const user = await helpers.findBy(email).first();
+            if (user) {
+                return res.status(409).json({
+                    errorMessage: `${email} is already registered`
+                })
+            }
+        const newUser = await helpers.createUser(req.body);
+        res.status(201).json(newUser)
+    } catch (e) {
+        console.log(e);
+        next();
+    }
+});
+
+module.exports = router;
