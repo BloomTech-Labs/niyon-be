e214d18ad253233bf51dda589a1b4192260633b3
timislearning <tim.mitzen@gmail.com>
2020-05-20 17:33:14 -0700

    More test
    Co-authored-by: Joe Thompson bmo3696@gmail.com
    Co-authored-by: Tim Mitzen tim.mitzen@gmail.com

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,7 @@ const server = require("../server");
 test("/ working", async () => {
   const res = await supertest(server).get("/");
   expect(res.status).toBe(200);
-  expect(res.body.welcomeMessage).toBe("Welcome to the Niyon Backend");
+  expect(res.body.welcomeMessage).toBe("Welcome to the auto deployed Niyon Server");
 });
 
 test("/ not working", async () => {
