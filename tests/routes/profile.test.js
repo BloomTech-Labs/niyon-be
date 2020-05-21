a44f26e8a231775e5f58e2a9e6111176a5046672
timislearning <tim.mitzen@gmail.com>
2020-05-21 16:02:36 -0700

    more test
    Co-authored-by: Joe Thompson <bmo3696@gmail.com>
    Co-authored-by: Tim Mitzen <tim.mitzen@gmail.com>
    '

--- tests/routes/profile.test.js
+++ tests/routes/profile.test.js
@@ -1,19 +1,33 @@
 const supertest = require("supertest");
 const server = require("../../server");
 const db = require("../../db/config");
+const restricted =require('../../Middleware/restricted')
 
-test("api not working", async () => {
+test("profilePackage:Id not working", async () => {
   const res = await supertest(server).put("/profilePackage/:id");
   expect(res.status).toBe(404);
+  
 });
 
 test("api not working", async () => {
-  const res = await supertest(server).put("/profilePackage");
-  expect(res.status).toBe(404);
+  const res = await supertest(server).put("/profilePackage",restricted(), async(res,req,next) =>{
+     try{
+   const profile_starter={
+      techs,
+      location,
+      jobs}
+      expect(res.status).toBe(200).json(profile_starter);
+   }
+   catch{
+      next()
+   }
+  });
+ 
 });
 
 test("api not working", async () => {
   const res = await supertest(server).get("/:id");
   expect(res.status).toBe(404);
 });
+
 
