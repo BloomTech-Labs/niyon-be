6abbe85e4681eb0ccf99e6d4abe5878cfb7c6491
Joe-Thompson <bmo3696@gmail.com>
2020-05-26 18:33:10 -0400

    deleted PUT request, created an allUsers Route, updated README
    Co-authored-by: Joe Thompson <bmo3696@gmail.com>
    Co-authored-by: Tim Mitzen <tim.mitzen@gmail.com>

--- Routers/ProfileRouter/ProfilePackage.js
+++ Routers/ProfileRouter/ProfilePackage.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { userHelper, jobHelper, locationHelper, techHelper } = require('../../models/classHelpers');
 const helpers = require('../../models/index');
 const restricted = require('../../Middleware/restricted');
 
@@ -23,70 +24,47 @@ router.get('/profilePackage',restricted(), async (req, res, next) => {
     }
 });
 
-router.put('/profilePackage/:id', restricted(), async (req, res, next) => {
-    try {
-        const user_id = req.params.id
-        const { techs, location_id, job_title, first_name, last_name, bio } = req.body;
-        const findUser = await helpers.user.findById(user_id);
-            if (findUser.first_name === null) {
-                await helpers.user.update(user_id, {first_name})
+router.get('/', restricted(), async (req, res, next) => {
+   try {
+    const allUsers = await userHelper.getAll();
+    async function userData(arr) {
+        try {
+            if (!arr.job_title_id) {
+                arr.job_title_id = 1
             }
-            if (findUser.last_name === null) {
-                await helpers.user.update(user_id, {last_name})
+            if (!arr.location_id) {
+                arr.location_id = 1
             }
-            if (findUser.job_title_id === null) {
-                await helpers.user.update(user_id, {job_title_id: job_title})
-            }
-            if (findUser.location_id === null) {
-                await helpers.user.update(user_id, {location_id})
-            }
-            if (findUser.bio === null) {
-                await helpers.user.update(user_id, {bio})
-            }
+            const job = await jobHelper.findById(arr.job_title_id);
+            const location = await locationHelper.findById(arr.location_id);
+            const techs = await techHelper.userTech(arr.id);
+            delete arr.password;
 
-        techs.forEach((arr) => {
-            const tech = helpers.tech.updateTech(user_id, arr)
-                .then(res => {
-                    console.log('promise resolved successfully')
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        })
-
-        const getTitle = await helpers.job_title.getById(job_title);
-        const getLocation = await helpers.location.getById(location_id)
-        const tech_stack = [];
-        techs.forEach((arr) => {
-            const tech = helpers.tech.getById(arr)
-                .then(res => {
-                    console.log('promise resolved successfully')
-                    tech_stack.push(res)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        })
-        const updatedUser = await helpers.user.findById(user_id)
-        const returnedUser = {
-                ...updatedUser,
-            job_title: getTitle.job_title,
-            location: getLocation.location,
-            techs: tech_stack
+            return {
+                ...arr,
+                job: job.job_title,
+                location: location.location,
+                tech_stack: techs
+            }
+        } catch (e) {
+            console.log(e)
         }
-        // deleting password for security
-        delete returnedUser.password
-        res.status(201).json(returnedUser)
-    } catch (e) {
-        console.log(e)
-        next()
     }
+    const getData = async () => {
+        return Promise.all(allUsers.map(arr => userData(arr)))
+    }
+    getData().then(data => {
+        res.status(200).json(data)
+    })
+   } catch (e) {
+       next(e);
+   }
 });
 
 router.get('/:id', restricted(), async (req, res, next) => {
    try {
         const user_id = req.params.id;
-        const user = await helpers.user.findById(user_id);
+        const user = await userHelper.findById(user_id);
             if (!user) {
                 res.status(404).json({
                     errorMessage: `User with the id of ${user_id} was not found`
@@ -95,15 +73,13 @@ router.get('/:id', restricted(), async (req, res, next) => {
         if (!user.job_title_id) {
             user.job_title_id = 1
         }
-        const job = await helpers.job_title.getById(user.job_title_id);
+        const job = await jobHelper.findById(user.job_title_id);
+
         if (!user.location_id) {
             user.location_id = 1
         }
-        const location = await helpers.location.getById(user.location_id);
-            if (!location.location) {
-                location.location = 1
-            }
-        const techs = await helpers.tech.userTech(user_id);
+        const location = await locationHelper.findById(user.location_id);
+        const techs = await techHelper.userTech(user.id);
 
         const returnedUser = {
             ...user,
@@ -124,20 +100,45 @@ router.get('/:id', restricted(), async (req, res, next) => {
 router.post('/:id',restricted(), async (req, res, next) => {
     try {
         const user_id = req.params.id;
-        const validateUser = await helpers.user.findById(user_id);
+        const validateUser = await userHelper.findById(user_id);
             if (!validateUser) {
                 res.status(400).json({
                     errorMessage: 'User ID not found'
                 })
             }
-        const data = req.body;
+        const { techs } = req.body;
+            if ( techs ) {
+                techs.map((arr) => {
+                    techHelper.updateTech(user_id, arr)})
+            }
+        const { first_name, last_name, bio, email, job_title_id, location_id }  = req.body;
+        const data = {
+            first_name,
+            last_name,
+            bio,
+            email,
+            job_title_id,
+            location_id
+        }
             if (!data) {
                 res.status(400).json({
                     errorMessage: "No data included in request"
                 })
             }
-        const update = await helpers.user.update(user_id, data)
-        res.status(201).json(update)
+        await userHelper.update(user_id, data);
+        const user = await userHelper.findById(user_id);
+        const job = await jobHelper.findById(user.job_title_id);
+        const location = await locationHelper.findById(user.location_id);
+        const tech = await techHelper.userTech(user.id);
+
+        const returnedUser = {
+            ...user,
+            job,
+            location,
+            tech
+        }
+        delete returnedUser.password
+        res.status(201).json(returnedUser)
     } catch (e) {
         console.log(e)
         next()
