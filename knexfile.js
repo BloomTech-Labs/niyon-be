f361e471da9a9f82675ece40c69ced5741912f94
Joe Thompson <51089982+Joe-Thompson@users.noreply.github.com>
2020-06-02 22:45:56 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   test: {
     client: "pg",
-    connection: process.env.HEROKU_POSTGRESQL_ONYX_URL,
+    connection: "postgres://lqtdmycffkmufv:e5cd313f3acb6780ddf83a3dffe9eee55d27b3fefc57163f8b559401b6418c8f@ec2-18-210-214-86.compute-1.amazonaws.com:5432/d8jg2nh2jskl47",
 
     seeds: {
       directory: "./db/test/seeds",
